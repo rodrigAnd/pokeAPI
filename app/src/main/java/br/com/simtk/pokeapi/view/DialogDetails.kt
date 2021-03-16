c5c184dd60f4d68e58a9946a7bef84df2cd619bb
Rodrigo R. Guilherme <rodrigo.r.guilherme@outlook.com>
2021-03-16 11:55:21 -0300

    detalhes pokemons implementar

--- app/src/main/java/br/com/simtk/pokeapi/view/DialogDetails.kt
+++ app/src/main/java/br/com/simtk/pokeapi/view/DialogDetails.kt
@@ -8,5 +8,7 @@ class DialogDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dialog_details)
+
+        //implementan detalhes obtidos da lista de pokemons
     }
 }
